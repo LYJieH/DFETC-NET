8ecef7c0222a665078254632b619950aece8fd74
LYJieH <146905039+LYJieH@users.noreply.github.com>
2023-10-09 00:24:42 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,67 @@
+
+import os
+import numpy as np
+import cv2
+import json
+from glob import glob
+from metrics import *
+from sklearn.utils import shuffle
+from tensorflow.keras.utils import CustomObjectScope
+from tensorflow.keras.models import load_model
+from model_scunet import *
+
+def create_dir(path):
+    """ Create a directory. """
+    try:
+        if not os.path.exists(path):
+            os.makedirs(path)
+    except OSError:
+        print(f"Error: creating directory with name {path}")
+
+def read_data(x, y):
+    """ Read the image and mask from the given path. """
+    image = cv2.imread(x, cv2.IMREAD_COLOR)
+    mask = cv2.imread(y, cv2.IMREAD_COLOR)
+    return image, mask
+
+def read_params():
+    """ Reading the parameters from the JSON file."""
+    with open("params.json", "r") as f:
+        data = f.read()
+        params = json.loads(data)
+        return params
+
+def load_data(path):
+    """ Loading the data from the given path. """
+    images_path = os.path.join(path, "image/*")
+    masks_path  = os.path.join(path, "mask/*")
+
+    images = glob(images_path)
+    masks  = glob(masks_path)
+
+    return images, masks
+
+def shuffling(x, y):
+    x, y = shuffle(x, y, random_state=42)
+    return x, y
+
+def load_model_weight(path):
+    with CustomObjectScope({
+        'dice_loss': dice_loss,
+        'dice_coef': dice_coef,
+        'bce_dice_loss': bce_dice_loss,
+        'iou': iou
+        }):
+
+
+        model = load_model(path)
+
+    return model
+
+def get_initial_weights(output_size):
+    b = np.zeros((2, 3), dtype='float32')
+    b[0, 0] = 1
+    b[1, 1] = 1
+    W = np.zeros((output_size, 6), dtype='float32')
+    weights = [W, b.flatten()]
+    return weights
